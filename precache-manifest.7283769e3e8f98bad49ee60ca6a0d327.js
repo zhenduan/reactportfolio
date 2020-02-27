5c203d2e84c419bfb0cef70021bd7ef1e4fbf6a7
Zhen <duan2439@gmail.com>
2020-02-27 16:01:10 +0800

    Updates

---
+++ precache-manifest.7283769e3e8f98bad49ee60ca6a0d327.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63b27b427406f600c0db5091fe167d2f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "afef9c6547c1ba26555a",
+    "url": "/static/css/2.a6bc1c31.chunk.css"
+  },
+  {
+    "revision": "810ab0b9d06a6cd6c1c9",
+    "url": "/static/css/main.d860f6ef.chunk.css"
+  },
+  {
+    "revision": "afef9c6547c1ba26555a",
+    "url": "/static/js/2.2d691e57.chunk.js"
+  },
+  {
+    "revision": "8956eb797ec1ca27a54b67665871977c",
+    "url": "/static/js/2.2d691e57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "810ab0b9d06a6cd6c1c9",
+    "url": "/static/js/main.2a714d9c.chunk.js"
+  },
+  {
+    "revision": "0d46f7656c748826843c",
+    "url": "/static/js/runtime-main.c2b20a0b.js"
+  }
+]);
